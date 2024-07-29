ef489e0ab78958844ad3f8359e90755de3d5018b
Abraham Zamora <zamoraab@oregonstate.edu>
2024-07-28 19:35:31 -0700

    clean up test files. added instructions. added makefile

--- tests/opcodes6502.c
+++ tests/opcodes6502.c
@@ -8,168 +8,19 @@
 #include "flags.h"
 #include "helpers6502.h"
 
+/**
+ * 
+ * Instructions:
+ * From your implementation in the emulator.c file, add in your opcode functions that you want to test (simple copy/paste).
+ * Remove the static keyword from each function. The opcodes.h file already has every opcode function declared to make this easier.
+ * 
+ * 
+ */
 
-// ================== opcode functions ======================================
 
-// James opcode functions
-static void execute_0x00(State6502* state) {
-	// more details on the BRK instruction can be found here: 
-	// http://www.6502.org/tutorials/interrupts.html#2.2
-	
-	fprintf(stdout, "Executing opcode 0x00: BRK\n");
-	
-	// push the program counter to the stack
-	// according to https://en.wikipedia.org/wiki/Interrupts_in_65xx_processors,
-	// 2 is added to the program counter prior to pushing to the stack.
-	// little endian - addresses stored in memory with LSB first
-	uint16_t tmp_pc = state->pc + 2;
-	unsigned char pc_bytes[] = {tmp_pc >> 8, tmp_pc & 0x00FF};
-	int size = sizeof(pc_bytes);
-	int push_result = push_stack(state, size, pc_bytes);
-	if (push_result < 0) {
-		// error in call to push_stack
-		return;
-	}
-
-	// push the processor status to the stack
-	update_processor_status(state);
-	// size is 2 because by default a full word (16 bits) should be pushed at
-	// a time.
-	size = 2;
-	unsigned char processor_status_bytes[] = {0x00, state->processor_status};
-	push_result = push_stack(state, size, processor_status_bytes); 
-	if (push_result < 0) {
-		// error in call to push_stack
-		return;
-	}
-
-	// load the BRK/IRQ vector into the program counter.
-	// I don't see anything re. an initial value for 0xFFFE-0xFFFF on power up:
-	// https://www.nesdev.org/wiki/CPU_power_up_state
-	state->pc = (state->memory[0xFFFF] << 8) | (state->memory[0xFFFE]);
-
-	// set the break flag
-	state->flgs->brk_flag = 0x01;
-
-	// also set the interrupt disable flag as per 
-	// http://www.6502.org/tutorials/interrupts.html#2.2
-	state->flgs->inter_disable_flag = 0x01;
-
-	// TODO: re-examine this
-	// We made a decision to exit the program if an interrupt is encountered.
-	state->exit_prog = true;
-	
-}
-
-static void execute_0x01(State6502* state) {
-	fprintf(stdout, "Executing opcode 0x01: ORA - (indirect, X)\n");
-	state->pc++;
-	unsigned char zero_page_addr = state->memory[state->pc];
-
-	// As per http://www.emulator101.com/6502-addressing-modes.html,
-	// there is wrap around to an address on the 'zero page'.
-	uint16_t addr_of_addr = (state->x + zero_page_addr) & 0x00FF;
-	unsigned char addr_bytes[] = {state->memory[addr_of_addr], state->memory[++addr_of_addr]}; 
-
-	// 16 bit addresses are stored in little endian order
-	uint16_t addr = (addr_bytes[1] << 8) | addr_bytes[0];
-
-	unsigned char byte_to_or = state->memory[addr];
-
-	// inclusive OR on accumulator contents.
-	state->a |= byte_to_or;
-
-	// set zero flag if applicable.
-	if (state->a == 0x00) {
-		state->flgs->zro_flag = 1;
-	}
-
-	// set negative flag if bit 7 is set.
-	if ((state->a & 0x80) == 0x80) {
-		state->flgs->neg_flag = 1;
-	}
-}
-static void execute_0x05(State6502* state) {
-	fprintf(stdout, "Executing opcode 0x05: ORA - zero page\n");
-	state->pc++;
-	unsigned char zero_page_addr = state->memory[state->pc];
-	unsigned char byte_to_or = state->memory[zero_page_addr];
-
-	// inclusive OR on accumulator contents.
-	state->a |= byte_to_or;
-
-	// set zero flag if applicable.
-	if (state->a == 0x00) {
-		state->flgs->zro_flag = 1;
-	}
-
-	// set negative flag if bit 7 is set.
-	if ((state->a & 0x80) == 0x80) {
-		state->flgs->neg_flag = 1;
-	}
-}
-
-static void execute_0x06(State6502* state) {
-	fprintf(stdout, "Executing opcode 0x06: ASL - zero page\n");
-	state->pc++;
-	unsigned char zero_page_addr = state->memory[state->pc];
-	unsigned char selected_byte = state->memory[zero_page_addr];
-	unsigned char op_result = (selected_byte << 1);
-	uint8_t old_bit7 = (selected_byte & 0x80) == 0x80 ? 0x01 : 0x00;
-	state->memory[zero_page_addr] = op_result;
-
-	state->flgs->crry_flag = old_bit7;
-	
-	// The author of the guide here (https://www.nesdev.org/obelisk-6502-guide/reference.html#ASL)
-	// indicated that the zero flag should be set if the result of the instruction 
-	// applied to its operand is zero, not (as it states in the guide) the 
-	// accumulator. 
-	// See the author's comment here: http://forum.6502.org/viewtopic.php?f=12&t=5351
-	if (op_result == 0x00) {
-		state->flgs->zro_flag = 0x01;
-	}
-}
-
-static void execute_0x08(State6502* state) {
-	// This opcode just pushes the processer_status register to the stack.
-	fprintf(stdout, "Executing opcode 0x08: PHP - Implied\n");
-	update_processor_status(state);
-	int size = 2;
-	unsigned char processor_status_bytes[] = {0x00, state->processor_status};
-	int push_result = push_stack(state, size, processor_status_bytes); 
-	if (push_result < 0) {
-		// error in call to push_stack
-		return;
-	}
-}
-
-static void execute_0x09(State6502* state) {
-	// TODO
-	fprintf(stdout, "Executing opcode 0x09: ORA - Immediate\n");
-	state->pc++;
-	unsigned char byte_to_or = state->memory[state->pc];
-
-	// inclusive OR on accumulator contents.
-	state->a |= byte_to_or;
-
-	// set zero flag if applicable.
-	if (state->a == 0x00) {
-		state->flgs->zro_flag = 1;
-	}
-
-	// set negative flag if bit 7 is set.
-	if ((state->a & 0x80) == 0x80) {
-		state->flgs->neg_flag = 1;
-	}
-}
-
-// Chris' opcode functions
-static void execute_0x2c(State6502* state) {
-	// TODO
-}
 
 // Abraham opcode functions
-static void execute_0x58(State6502* state) {
+void execute_0x58(State6502* state) {
     // Opccode 0x58: CLI - Clear Interrupt Disable Flag
     fprintf(stdout, "Executing opcode 0x58: CLI\n");
     // clearing the interrupt disable flag
@@ -179,7 +30,7 @@ static void execute_0x58(State6502* state) {
 
 }
 
-static void execute_0x59(State6502* state) {
+void execute_0x59(State6502* state) {
     // Opccode 0x59: EOR - Exclusive OR Absolute Y
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#EOR
     
@@ -207,7 +58,7 @@ static void execute_0x59(State6502* state) {
 
 }
 
-static void execute_0x5d(State6502* state) {
+void execute_0x5d(State6502* state) {
     // Opccode 0x5D: EOR - Exclusive OR Absolute X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#EOR
     
@@ -235,7 +86,7 @@ static void execute_0x5d(State6502* state) {
 
 }
 
-static void execute_0x5e(State6502* state) {
+void execute_0x5e(State6502* state) {
     // Opccode 0x5E: LSR - Logical Shift Right Absolute X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#LSR
     
@@ -267,7 +118,7 @@ static void execute_0x5e(State6502* state) {
 
 }
 
-static void execute_0x60(State6502* state) {
+void execute_0x60(State6502* state) {
     // Opccode 0x60: RTS - Return from Subroutine
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#RTS
     
@@ -288,7 +139,7 @@ static void execute_0x60(State6502* state) {
 
 }
 
-static void execute_0x61(State6502* state) {
+void execute_0x61(State6502* state) {
     // Opccode 0x61: ADC - Add with Carry Indirect X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x61: ADC\n");
@@ -328,7 +179,7 @@ static void execute_0x61(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x65(State6502* state) {
+void execute_0x65(State6502* state) {
     // Opccode 0x65: ADC - Add with Carry Zero Page
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x65: ADC\n");
@@ -355,7 +206,7 @@ static void execute_0x65(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x66(State6502* state) {
+void execute_0x66(State6502* state) {
     // Opccode 0x66: ROR - Rotate Right Zero Page
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ROR
     fprintf(stdout, "Executing opcode 0x66: ROR\n");
@@ -384,7 +235,7 @@ static void execute_0x66(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x68(State6502* state) {
+void execute_0x68(State6502* state) {
     // Opccode 0x68: PLA - Pull Accumulator
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#PLA
     fprintf(stdout, "Executing opcode 0x68: PLA\n");
@@ -410,7 +261,7 @@ static void execute_0x68(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x69(State6502* state) {
+void execute_0x69(State6502* state) {
     // Opccode 0x69: ADC - Add with Carry Immediate
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x69: ADC\n");
@@ -437,7 +288,7 @@ static void execute_0x69(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x6a(State6502* state) {
+void execute_0x6a(State6502* state) {
     // Opccode 0x6A: ROR - Rotate Right Accumulator
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ROR
     fprintf(stdout, "Executing opcode 0x6A: ROR\n");
@@ -459,7 +310,7 @@ static void execute_0x6a(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x6c(State6502* state) {
+void execute_0x6c(State6502* state) {
     // Opccode 0x6C: JMP - Jump Indirect
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#JMP
     fprintf(stdout, "Executing opcode 0x6C: JMP\n");
@@ -473,7 +324,7 @@ static void execute_0x6c(State6502* state) {
 
 }
 
-static void execute_0x6d(State6502* state) {
+void execute_0x6d(State6502* state) {
     // Opccode 0x6D: ADC - Add with Carry Absolute
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x6D: ADC\n");
@@ -503,7 +354,7 @@ static void execute_0x6d(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x6e(State6502* state) {
+void execute_0x6e(State6502* state) {
     // Opccode 0x6E: ROR - Rotate Right Absolute
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ROR
     fprintf(stdout, "Executing opcode 0x6E: ROR\n");
@@ -533,7 +384,7 @@ static void execute_0x6e(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x70(State6502* state) {
+void execute_0x70(State6502* state) {
     // Opccode 0x70: BVS - Branch if Overflow Set
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#BVS
     fprintf(stdout, "Executing opcode 0x70: BVS\n");
@@ -552,7 +403,7 @@ static void execute_0x70(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x71(State6502* state) {
+void execute_0x71(State6502* state) {
     // Opccode 0x71: ADC - Add with Carry Indirect Y
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x71: ADC\n");
@@ -592,7 +443,7 @@ static void execute_0x71(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x75(State6502* state) {
+void execute_0x75(State6502* state) {
     // Opccode 0x75: ADC - Add with Carry Zero Page X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x75: ADC\n");
@@ -619,7 +470,7 @@ static void execute_0x75(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x76(State6502* state) {
+void execute_0x76(State6502* state) {
     // Opccode 0x76: ROR - Rotate Right Zero Page X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ROR
     fprintf(stdout, "Executing opcode 0x76: ROR\n");
@@ -648,7 +499,7 @@ static void execute_0x76(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x78(State6502* state) {
+void execute_0x78(State6502* state) {
     // Opccode 0x78: SEI - Set Interrupt Disable Flag
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#SEI
     fprintf(stdout, "Executing opcode 0x78: SEI\n");
@@ -659,7 +510,7 @@ static void execute_0x78(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x79(State6502* state) {
+void execute_0x79(State6502* state) {
     // Opccode 0x79: ADC - Add with Carry Absolute Y
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x79: ADC\n");
@@ -692,7 +543,7 @@ static void execute_0x79(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x7d(State6502* state) {
+void execute_0x7d(State6502* state) {
     // Opccode 0x7D: ADC - Add with Carry Absolute X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ADC
     fprintf(stdout, "Executing opcode 0x7D: ADC\n");
@@ -725,7 +576,7 @@ static void execute_0x7d(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x7e(State6502* state) {
+void execute_0x7e(State6502* state) {
     // Opccode 0x7E: ROR - Rotate Right Absolute X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#ROR
     fprintf(stdout, "Executing opcode 0x7E: ROR\n");
@@ -758,7 +609,7 @@ static void execute_0x7e(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x81(State6502* state) {
+void execute_0x81(State6502* state) {
     // Opccode 0x81: STA - Store Accumulator Indirect X
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#STA
     fprintf(stdout, "Executing opcode 0x81: STA\n");
@@ -777,11 +628,11 @@ static void execute_0x81(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x84(State6502* state) {
+void execute_0x84(State6502* state) {
 	// TODO
 }
 
-static void execute_0x85(State6502* state) {
+void execute_0x85(State6502* state) {
     // Opccode 0x85: STA - Store Accumulator Zero Page
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#STA
     fprintf(stdout, "Executing opcode 0x85: STA\n");
@@ -797,7 +648,7 @@ static void execute_0x85(State6502* state) {
     state->pc++;
 }
 
-static void execute_0x86(State6502* state) {
+void execute_0x86(State6502* state) {
     // Opccode 0x86: STX - Store X Register Zero Page
     // https://www.nesdev.org/obelisk-6502-guide/reference.html#STX
     fprintf(stdout, "Executing opcode 0x86: STX\n");
